32c30b19bcb5f2a8c0307baeeecfe8df8b90b2c1
YunGyeongee <yuuung26@gmail.com>
2021-08-26 07:58:43 +0900

    연산자1

---
+++ 03_Operation/src/com/yg/operator/B_Arithmetic.java
@@ -0,0 +1,17 @@
+package com.yg.operator;
+
+public class B_Arithmetic {
+
+	public void method() {
+		int num1 = 10;
+		int num2 = 3;
+		
+		System.out.println("num1 + num2 : " + (num1 + num2));
+		System.out.println("num1 - num2 : " + (num1 - num2));
+		System.out.println("num1 x num2 : " + (num1 * num2));
+		System.out.println("num1 / num2 : " + (num1 / num2)); // 나누기의 몫
+		System.out.println("num1 % num2 : " + (num1 % num2)); // 나누기의 나머지(나머지 연산자)
+		
+	}
+	
+}
---
+++ 03_Operation/src/com/yg/run/OperatorRun.java
@@ -0,0 +1,18 @@
+package com.yg.run;
+
+import com.yg.operator.A_LogicalNegation;
+import com.yg.operator.B_Arithmetic;
+
+public class OperatorRun {
+	
+	public static void main(String[] args) {
+		
+		A_LogicalNegation a = new A_LogicalNegation();
+		//a.method();
+		
+		B_Arithmetic b = new B_Arithmetic();
+		b.method();
+		
+	}
+
+}
